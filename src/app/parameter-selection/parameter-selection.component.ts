7908237bf2dc6a26bbc9abafc4283ab31c48971c
Merz, Katharina <Katharina.Merz@berner-group.com>
2020-12-16 17:11:29 +0100

    added simulation interface

--- src/app/parameter-selection/parameter-selection.component.ts
+++ src/app/parameter-selection/parameter-selection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Simulation } from '../simulation';
+
 
 @Component({
   selector: 'app-parameter-selection',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParameterSelectionComponent implements OnInit {
 
+    simulation: Simulation = {
+      rundenanzahl: 1000,
+      eventWahrscheinlichkeit: 50,
+      lieferantenanzahl: 50,
+      produktionsunternehmenanzahl: 50,
+      kundenanzahl: 300,
+      // produktpalette: produktpalette;
+      startKapitalLMin:  10000,
+      startKapitalLMax:  1000000,
+      startKapitalPUMin: 10000,
+      startKapitalPUMax: 1000000,
+      startKapitalKMin: 10,
+      startKapitalKMax: 10000,
+      produktionsmengePUMin: 40,
+      produktionsmengePUMax: 2000,
+      produktionsmengeLMin: 20,
+      produktionsmengeLMax: 8000,
+    };
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
---
+++ src/app/simulation.ts
@@ -0,0 +1,18 @@
+export interface Simulation {
+    rundenanzahl: number;
+    eventWahrscheinlichkeit: number;
+    lieferantenanzahl: number;
+    kundenanzahl: number;
+    // produktpalette: produktpalette;
+    startKapitalKMax: number;
+    startKapitalKMin: number;
+    produktionsunternehmenanzahl: number;
+    startKapitalPUMin: number;
+    startKapitalPUMax: number;
+    startKapitalLMin: number;
+    startKapitalLMax: number;
+    produktionsmengePUMin: number;
+    produktionsmengePUMax: number;
+    produktionsmengeLMin: number;
+    produktionsmengeLMax: number;
+}
