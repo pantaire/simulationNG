b8276d56215b3af6c0c90b9e542f1dd73a52e745
Merz, Katharina <Katharina.Merz@berner-group.com>
2021-01-07 19:20:14 +0100

    deleted alert of input data

--- src/app/parameter-selection/parameter-selection.component.ts
+++ src/app/parameter-selection/parameter-selection.component.ts
@@ -28,7 +28,6 @@ export class ParameterSelectionComponent implements OnInit {
       console.log(result,"result")
     })
     console.warn(simulationInput);
-    alert("Entered data: " + simulationInput)
     this.router.navigate(['dashboard']);
   }
 }
