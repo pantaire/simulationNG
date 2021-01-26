04ede07b770b31af0965bb932c3690587cb6bed1
Merz, Katharina <Katharina.Merz@berner-group.com>
2021-01-26 16:12:50 +0100

    added button id to expertmode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ExpertModeComponent } from './expert-mode/expert-mode.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ParameterSelectionComponent } from './parameter-selection/parameter-selection.component';
 import { SimulationDashboardComponent } from './simulation-dashboard/simulation-dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
 import { WeatherComponent } from './weather/weather.component';
-import { ExpertModeComponent } from './expert-mode/expert-mode.component';
 
 @NgModule({
   declarations: [
     AppComponent,  
+    ExpertModeComponent,
     FooterComponent,
     HeaderComponent,
     ParameterSelectionComponent,
     SimulationDashboardComponent,
-    WeatherComponent,
-    ExpertModeComponent
+    WeatherComponent
   ],  
   imports: [
     HttpClientModule,  
